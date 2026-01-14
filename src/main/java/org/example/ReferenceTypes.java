101bbd5186ea0b83005b0b23a9604e3c4b69b0a9
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-14 16:54:30 +0300

    CReate  ReferenceTypes.java

--- src/main/java/org/example/ReferenceTypes.java
+++ src/main/java/org/example/ReferenceTypes.java
@@ -1,4 +1,5 @@
-package org.example;
+//equals
+//==
 
-public class ReferenceTypes {
-}
+//The == operator is used to compare primitive values or to check if two object references point to the same memory location. The .equals() method is used to compare
+// the actual contents or values of objects, especially when working with objects like strings.
