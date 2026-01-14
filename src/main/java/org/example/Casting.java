bcb0e68c9f483f0672db5e18a6c66239b2aec1a0
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-14 16:52:55 +0300

    Commit Casting changes

--- src/main/java/org/example/Casting.java
+++ src/main/java/org/example/Casting.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class Casting {
+    int myInt = 10; //Higher
+    double myDouble = myInt; //Implicit Casting(Widening)
+    short myShort = (short) myInt; //Lower -> Explicit casting(Narrowing)
+
+    long myLong = (long) myDouble;
+
+    float myFloat = (float) myDouble;
+
+    public void cast(){
+        System.out.println(myShort);
+    }
+   //byte --> short --> int --> float --> double --> long
 }
