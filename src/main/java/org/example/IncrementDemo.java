8d50a43b3a6bef8eef880af9f75693e7b74cae89
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-14 16:53:31 +0300

    Commit IncrementDemo.java

--- src/main/java/org/example/IncrementDemo.java
+++ src/main/java/org/example/IncrementDemo.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class IncrementDemo {
+     int num = 10;
+
+     public void increment(){
+         System.out.println("Count: " + num--); //10
+         System.out.println("Count: " + ++num); //10
+     }
 }
