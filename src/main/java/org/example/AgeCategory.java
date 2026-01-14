bdc29d0f25b5c4795bbd0b61b1d7983ed33996fd
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-14 17:28:22 +0300

    Create AgeCategory to understand control flow

---
+++ src/main/java/org/example/AgeCategory.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class AgeCategory {
+
+    public static void main(String[] args) {
+        displayAgeControl();
+    }
+
+
+    public static void displayAgeControl() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        
+        if(age <= 2){
+            System.out.println("You are an infant");
+        } else if (age >= 3 && age <= 12) {
+            System.out.println("You are a child");           
+        }else if(age >=13 && age <= 19){
+            System.out.println("You're a teenager");
+        } else if (age >= 20 && age <=64) {
+            System.out.println("You're an adult");
+            
+        }else if (age >=65){
+            System.out.println("You are a senior");
+        }
+
+
+    }
+}
