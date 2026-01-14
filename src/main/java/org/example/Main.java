c350eaf05701a106795995c0003b252be4a81f80
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-14 16:55:04 +0300

    Create UserInput class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//    Casting casting = new Casting();
+//    casting.cast();
+//        PrimitiveTypes types = new PrimitiveTypes();
+//        types.printY();
+
+//        IncrementDemo  demo = new IncrementDemo();
+//        demo.increment();
+
+        Calculator calculator = new Calculator();
+        //calculator.calculate();
+        calculator.studentInformation();
+
+
+    }
+}
--- src/main/java/org/example/UserInput.java
+++ src/main/java/org/example/UserInput.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.Scanner; // Import required
 public class UserInput {
+    public static void main(String[] args) {
+// Create Scanner object
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine(); // Read String input
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt(); // Read integer input
+        System.out.println("Hello " + name + ", you are " + age + " years old!");
+        scanner.close(); // Always close scanner when done
+    }
 }
+
