249d602ea55782446bdbccc518ae51b64e07d255
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-15 16:38:20 +0300

    Create Main in abstraction folder

--- src/main/java/abstraction/Main.java
+++ src/main/java/abstraction/Main.java
@@ -1,4 +1,14 @@
 package abstraction;
 
 public class Main {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.makeSound();
+        myCat.printName("Fluffy");
+
+        Dog myDog = new Dog();
+        myDog.makeSound();
+        myDog.printName("Bob");
+    }
+
 }
