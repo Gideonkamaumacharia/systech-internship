5f902b65b5f2e393074a3b7806b65a55df6e0746
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-15 10:44:41 +0300

    Commit all changes

--- src/main/java/javaSECert/HelloWorld.java
+++ src/main/java/javaSECert/HelloWorld.java
@@ -1,4 +1,14 @@
 package javaSECert;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello");
+        int A = 20;
+        String D = "Hello";
+
+        System.out.println(stringBuilder.equals(D)); //This prints false coz stringBuilder is a StringBuilder
+        //object and we are trying to compare a StringBuilder object to a String object
+        System.out.println(stringBuilder.toString().equals(D));//This prints true coz we have converted the stringBuilder
+        //object to string object and now we are comparing the contents ie "Hello" vs "Hello"
+    }
 }
