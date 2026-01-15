44d1e27b69741ebaf52c76555f14bcc4992c0fce
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-15 10:43:56 +0300

    Create HelloWorld2.java

---
+++ src/main/java/javaSECert/HelloWorld2.java
@@ -0,0 +1,13 @@
+package javaSECert;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+    }
+}
+//How you print "Hello moon?"
+    //javac invokes the compiler  .NB// javac doesnt compile class names,it compiles source files
+    //therefore we need to add .java extension in the command
+// __Commands__
+         //javac HelloWorld2.java
+         //java HelloWorld2.java moon
