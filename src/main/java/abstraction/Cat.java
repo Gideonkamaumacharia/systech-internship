acdefb79e094812bcd24316d24890da1eb66730c
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-15 16:36:39 +0300

    Create Cat class

--- src/main/java/abstraction/Cat.java
+++ src/main/java/abstraction/Cat.java
@@ -1,4 +1,15 @@
 package abstraction;
 
-public class Cat {
+public class Cat extends Animal implements AnimalInterface{
+
+    @Override
+    public void makeSound(){
+        System.out.println("Meow");
+    }
+
+    @Override
+    public void walk(){
+        System.out.println("Cat Walks");
+    }
+
 }
