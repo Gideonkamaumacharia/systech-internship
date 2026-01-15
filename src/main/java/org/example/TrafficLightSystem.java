a1514d56ce577fdd3690fa37b38fe8700ec55501
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-15 06:02:21 +0300

    Create TrafficLightSystem with traditional switch

---
+++ src/main/java/org/example/TrafficLightSystem.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Scanner;
+
+//Red: Stop
+//Yellow: Prepare to stop
+//Green: Go
+//Blinking Red: Treat as stop sign
+//Blinking Yellow: Proceed with caution
+public class TrafficLightSystem {
+    public static void main(String[] args) {
+        trafficLightSystem();
+    }
+
+    public static void trafficLightSystem(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the traffic light color: ");
+        String color = scanner.nextLine().toLowerCase();
+
+        String action;
+        switch (color){
+            case "red":
+                action = "Stop!";
+                break;
+            case "yellow":
+                action = "Prepare to stop!";
+                break;
+            case "green":
+                action = "Go!";
+                break;
+            case "blinking red":
+                action = "Treat as stop sign!";
+                break;
+            case "blinking yellow":
+                action = "Proceed with caution!";
+                break;
+            default:
+                action = "Error: Invalid color!";
+        }
+        System.out.println("Action: " + action);
+        scanner.close();
+    }
+
+}
