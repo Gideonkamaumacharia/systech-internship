f42b5ead1491e4d8136cbb1d92771653380138be
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-15 06:18:02 +0300

    Create SwitchExpressionTrafficSystem

---
+++ src/main/java/trafficLight/SWitchExpressionTrafficLightSystem.java
@@ -0,0 +1,29 @@
+package trafficLight;
+
+
+import java.util.Scanner;
+
+public class ModernSWitchTrafficLightSystem {
+    public static void main(String[] args) {
+        switchTrafficLightSystem();
+    }
+
+    public static void switchTrafficLightSystem(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the traffic light color: ");
+        String light = scanner.nextLine().toLowerCase();
+
+        String action;
+        switch (light) {
+            case "red" -> action = "Stop!";
+            case "yellow" -> action = "Prepare to stop!";
+            case "green" -> action = "Go!";
+            case "blinking red" -> action = "Treat as stop sign!";
+            case "blinking yellow" -> action = "Proceed with caution!";
+            default -> action = "Invalid light color";
+        }
+        System.out.println("Action: " + action);
+        scanner.close();
+    }
+}
