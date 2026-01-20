a5839b065b796448d0f46c9fc4e1019956e09f96
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-20 06:22:53 +0300

    Modify NumberSequence

--- src/main/java/Assessment1_GideonKamau/NumberSequence/NumberSequence.java
+++ src/main/java/Assessment1_GideonKamau/NumberSequence/NumberSequence.java
@@ -9,7 +9,7 @@ public class NumberSequence {
         int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         for (int number : numbers) {
             if (number % 5 == 0) {
-                System.out.println("Multiple of 5");
+                System.out.println("Odd,Multiple of 5");
             }else if (number % 2 == 0) {
                 System.out.println("Even");
             } else {
