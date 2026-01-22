f4cd5855dd174383938ab2d5c0e7597931e144d4
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-22 19:16:58 +0300

    Commit changes in generalExercises directory

--- src/main/java/javaSECertPrep/generalExercises/HelloWorld2.java
+++ src/main/java/javaSECertPrep/generalExercises/HelloWorld2.java
@@ -4,6 +4,30 @@ public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("Hello " + args[0]);
     }
+
+    public static class Car {
+        public String brand;
+        public String model;
+        public int year;
+    
+        public static void main(String[] args) {
+            //displayInfo();
+        }
+        // Constructor using this
+        public Car(String brand, String model, int year) {
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+        }
+        // Method using this
+        public  void displayInfo() {
+            System.out.println(this.year + " " + this.brand + " " + this.model);
+        }
+        // Method comparing objects using this
+        public boolean isNewerThan(Car otherCar) {
+            return this.year > otherCar.year;
+        }
+    }
 }
 //How you print "Hello moon?"
     //javac invokes the compiler  .NB// javac doesnt compile class names,it compiles source files
