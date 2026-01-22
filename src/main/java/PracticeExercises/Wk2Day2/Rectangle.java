27fb794760981d458c5d3aaece4caf41802a754a
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-22 19:17:54 +0300

    Commit all changes

--- src/main/java/PracticeExercises/Wk2Day2/Rectangle.java
+++ src/main/java/PracticeExercises/Wk2Day2/Rectangle.java
@@ -1,4 +1,24 @@
 package PracticeExercises.Wk2Day2;
 
 public class Rectangle {
+    public double width;
+    public double height;
+
+    //No args --> Default constructor
+    public Rectangle(){
+
+    }
+
+    //Constructor with width and height
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    //Constructor with single size (square)
+    public Rectangle(double size) {
+        this.width = size;
+        this.height = size;
+    }
+
 }
