191f5d37d1a008327e27c949ba9ec0e8a259ecdc
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-27 09:33:05 +0300

    Commit Class Shape

--- src/main/java/PracticeExercises/Wk3Day1/Shape/Shape.java
+++ src/main/java/PracticeExercises/Wk3Day1/Shape/Shape.java
@@ -1,4 +1,37 @@
 package PracticeExercises.Wk3Day1.Shape;
 
-public class Shape {
+public abstract class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape(String color,boolean filled){
+        if(color == null || color.trim().isEmpty()){
+            throw new IllegalArgumentException("Color cannot be null or empty!");
+        }
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    public abstract double getArea();
+    public abstract  double getPerimeter();
+
+    @Override
+    public String toString(){
+        return "This is the parent abstract class' toString implementation!";
+    }
 }
