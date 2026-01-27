40b2e33bddf0519ca71f77577e20183039f1afe7
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-27 09:33:40 +0300

    Commit all changes in Shape folder

--- src/main/java/PracticeExercises/Wk3Day1/Shape/Rectangle.java
+++ src/main/java/PracticeExercises/Wk3Day1/Shape/Rectangle.java
@@ -1,4 +1,60 @@
 package PracticeExercises.Wk3Day1.Shape;
 
-public class Rectangle {
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(String color, boolean filled, double width, double height) {
+        super(color, filled);
+        if(width <= 0){
+            throw new IllegalArgumentException("Width cannot be zero or negative");
+        }
+        this.width = width;
+        if(height <= 0){
+            throw new IllegalArgumentException("Height cannot be zero or negative");
+        }
+        this.height = height;
+    }
+
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                " width =" + width +
+                " height =" + height +
+                " area =" + getArea() +
+                " Perimeter =" + getPerimeter() +
+                '}';
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + height)* 2;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle("Red",false,12,15);
+        System.out.println(rectangle.toString());
+    }
 }
--- src/main/java/PracticeExercises/Wk3Day1/Shape/Triangle.java
+++ src/main/java/PracticeExercises/Wk3Day1/Shape/Triangle.java
@@ -1,4 +1,72 @@
 package PracticeExercises.Wk3Day1.Shape;
 
-public class Triangle {
+public class Triangle extends Shape{
+    private double base;
+    private double height;
+    private double hypotenuse;
+
+    public Triangle(String color, boolean filled, double base, double height,double hypotenuse) {
+        super(color, filled);
+        if(base <= 0){
+            throw new IllegalArgumentException("Base cannot be zero or negative");
+        }
+        this.base = base;
+        if(height <= 0){
+            throw new IllegalArgumentException("Height cannot be zero or negative");
+        }
+        this.height = height;
+        if(hypotenuse <= 0){
+            throw new IllegalArgumentException("Hypotenuse cannot be zero or negative");
+        }
+        this.hypotenuse = hypotenuse;
+
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHypotenuse() {
+        return hypotenuse;
+    }
+
+    public void setHypotenuse(double hypotenuse) {
+        this.hypotenuse = hypotenuse;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                " base =" + base +
+                " height =" + height +
+                " area =" + getArea() +
+                " Perimeter =" + getPerimeter() +
+                '}';
+    }
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 *(base * height);
+    }
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle("Red",false,12,15,8);
+        System.out.println(triangle.toString());
+    }
 }
