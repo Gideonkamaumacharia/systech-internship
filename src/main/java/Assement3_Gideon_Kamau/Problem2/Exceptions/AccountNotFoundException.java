b71b9f9695573c65af693753c259e784c464f880
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-31 16:51:48 +0300

    ../../Assement3_Gideon_Kamau/Problem2/Exceptions/AccountNotFoundException.java

--- src/main/java/Assement3_Gideon_Kamau/Problem2/Exceptions/AccountNotFoundException.java
+++ src/main/java/Assement3_Gideon_Kamau/Problem2/Exceptions/AccountNotFoundException.java
@@ -1,4 +1,7 @@
 package Assement3_Gideon_Kamau.Problem2.Exceptions;
 
-public class AccountNotFoundException {
+public class AccountNotFoundException extends Exception{
+    public AccountNotFoundException(String message){
+        super(message);
+    }
 }
