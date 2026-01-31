3cbcff21361b790c95ce3c3998d8fb77a65b9f1d
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-31 16:52:16 +0300

    ../../Assement3_Gideon_Kamau/Problem2/Exceptions/InsufficientFundsException.java

--- src/main/java/Assement3_Gideon_Kamau/Problem2/Exceptions/InsufficientFundsException.java
+++ src/main/java/Assement3_Gideon_Kamau/Problem2/Exceptions/InsufficientFundsException.java
@@ -1,4 +1,7 @@
 package Assement3_Gideon_Kamau.Problem2.Exceptions;
 
-public class InsufficientFundsException {
+public class InsufficientFundsException extends Exception{
+    public InsufficientFundsException(String message){
+        super(message);
+    }
 }
