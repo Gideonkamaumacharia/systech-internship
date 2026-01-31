0809b73ff6b639a6dd6591ad04be41c59930934b
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-01-31 16:53:09 +0300

    ../../Assement3_Gideon_Kamau/Problem2/Exceptions/InvalidTransactionException.java

--- src/main/java/Assement3_Gideon_Kamau/Problem2/Exceptions/InvalidTransactionException.java
+++ src/main/java/Assement3_Gideon_Kamau/Problem2/Exceptions/InvalidTransactionException.java
@@ -1,4 +1,7 @@
 package Assement3_Gideon_Kamau.Problem2.Exceptions;
 
-public class InvalidTransactionException {
+public class InvalidTransactionException extends RuntimeException{
+    public InvalidTransactionException(String message){
+        super(message);
+    }
 }
