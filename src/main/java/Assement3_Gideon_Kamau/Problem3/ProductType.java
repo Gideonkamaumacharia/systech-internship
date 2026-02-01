4512b92831aa0a7c6bf4ee53bb22619f24cabeee
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-01 03:48:47 +0300

    ../../Assement3_Gideon_Kamau/Problem3/ProductType.java

--- src/main/java/Assement3_Gideon_Kamau/Problem3/ProductType.java
+++ src/main/java/Assement3_Gideon_Kamau/Problem3/ProductType.java
@@ -1,4 +1,34 @@
 package Assement3_Gideon_Kamau.Problem3;
 
 public enum ProductType {
+    ELECTRONICS("Electronics"),
+    CLOTHING("Clothing"),
+    BOOKS("Books");
+
+    private final String displayName;
+
+    //Constructor
+    ProductType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    // The getter method
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    // Overriding toString to print nicely automatically
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
+
+
+//public Electronics(String id, String name, double price, int stock) {
+//    super(id, name, price, stock, ProductType.ELECTRONICS);
+//    // ... electronics specific fields ...
+//}
+
+
+
