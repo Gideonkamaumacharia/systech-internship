0fe5b1f91d49f8934cf300d7d5b61796ec66378b
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-05 03:08:01 +0300

    ../../javaSECertPrep/generalExercises/Udemy/Question1/Test.java

--- src/main/java/javaSECertPrep/generalExercises/Udemy/Question1/Test.java
+++ src/main/java/javaSECertPrep/generalExercises/Udemy/Question1/Test.java
@@ -1,4 +1,15 @@
 package javaSECertPrep.generalExercises.Udemy.Question1;
 
 public class Test {
+    public static void main(String[] args)throws Exception {
+        Game g = new Soccer();
+        //1
+        g.play();
+        Soccer a = (Soccer) g;
+        //2
+        a.play();
+        //Which of the given options can be inserted at 1 & 2?
+        //ANSWERS g.play() at 1 & a.play("cosco")      g.play() & a.play()
+        // In the second choice,Soccer is implictly overriding play() from Game(Parent)
+    }
 }
