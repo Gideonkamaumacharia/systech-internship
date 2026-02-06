66e4c7793d5207171cb77605369723aae749ba69
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-06 04:10:38 +0300

    ../../javaSECertPrep/generalExercises/WhizExercises/Exceptions/Whizlab.java

--- src/main/java/javaSECertPrep/generalExercises/WhizExercises/Exceptions/Whizlab.java
+++ src/main/java/javaSECertPrep/generalExercises/WhizExercises/Exceptions/Whizlab.java
@@ -1,4 +1,34 @@
 package javaSECertPrep.generalExercises.WhizExercises.Exceptions;
 
-public class Whizlab {
+class Whizlab {
+
+    public static void main(String args[]) {
+
+        try {
+
+            System.out.println(args[0]);
+
+        }catch (ArrayIndexOutOfBoundsException | ArithmeticException | NullPointerException e) {
+        //AS long as we are within the same block, the eception variable is implicitly final ,therefore we cannot assign the variable
+            //different value within the catch block.
+            if (e instanceof ArrayIndexOutOfBoundsException) {
+
+                e = new ArrayIndexOutOfBoundsException("Out of bounds");//Cannot assign a value to final variable 'e'
+
+            } else if(e instanceof NullPointerException) {
+
+                e = new NullPointerException("Null Value");//Cannot assign a value to final variable 'e'
+
+            } else {
+
+                e = new ArithmeticException("Arithmetic");//Cannot assign a value to final variable 'e'
+
+            }
+
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
 }
