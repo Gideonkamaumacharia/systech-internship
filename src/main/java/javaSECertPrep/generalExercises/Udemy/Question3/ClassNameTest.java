e82c25bbc5d1c2a790932f6caccd360cff3b0036
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-06 04:11:21 +0300

    ../../javaSECertPrep/generalExercises/Udemy/Question3/ClassNameTest.java

--- src/main/java/javaSECertPrep/generalExercises/Udemy/Question3/ClassNameTest.java
+++ src/main/java/javaSECertPrep/generalExercises/Udemy/Question3/ClassNameTest.java
@@ -1,4 +1,21 @@
 package javaSECertPrep.generalExercises.Udemy.Question3;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ClassNameTest {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        StringBuilder ab = new StringBuilder("mrx");
+        String string = ab.toString();
+        list.add(string);
+
+        System.out.println(string.getClass());//class java.lang.String
+
+        System.out.println(list.getClass());//class java.util.ArrayList
+
+    }
+
 }
