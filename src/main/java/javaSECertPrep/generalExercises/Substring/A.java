23fb0706a8e892c6007db7e2789919250a237977
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-07 22:42:38 +0300

    ../../javaSECertPrep/generalExercises/Substring

--- src/main/java/javaSECertPrep/generalExercises/Substring/A.java
+++ src/main/java/javaSECertPrep/generalExercises/Substring/A.java
@@ -1,4 +1,13 @@
 package javaSECertPrep.generalExercises.Substring;
 
 public class A {
+    static String x;
+    static float y;
+    static char z;
+
+    static void main() {
+        System.out.println(x); //prints null
+        System.out.println(y); //prints 0.0
+        System.out.println(z); //prints nothing
+    }
 }
--- src/main/java/javaSECertPrep/generalExercises/Substring/Main.java
+++ src/main/java/javaSECertPrep/generalExercises/Substring/Main.java
@@ -1,4 +1,22 @@
 package javaSECertPrep.generalExercises.Substring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    static void main() {
+        List< String> myList = new ArrayList<>();
+        myList.add("A");
+        myList.add("B");
+        myList.add("C");
+        myList.add("D");
+        myList.add("A");
+
+        myList.remove("B");
+        if(myList.remove("A")){
+            myList.remove("B");
+        }
+        System.out.println(myList);//prints CDA
+    }
 }
