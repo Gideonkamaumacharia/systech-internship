f70162c1081b1924238dd355b9397b434ed15dc9
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-09 21:32:03 +0300

    ../../javaSECertPrep/generalExercises/WhizExercises/Exceptions/ExceptionHandling.java

--- src/main/java/javaSECertPrep/generalExercises/WhizExercises/Exceptions/ExceptionHandling.java
+++ src/main/java/javaSECertPrep/generalExercises/WhizExercises/Exceptions/ExceptionHandling.java
@@ -1,4 +1,23 @@
 package javaSECertPrep.generalExercises.WhizExercises.Exceptions;
 
 public class ExceptionHandling {
+    public static void main(String[] args){
+        int ans;
+        try{
+
+            int num = 10;
+            int num1 = 0;
+
+            ans = num/num1;
+        }
+        catch( ArithmeticException ae){
+            ans = 0;
+
+        }
+        catch (Exception e){
+            System.out.println("INvalid calculation");
+        }
+        //System.out.println("Answer "+ ans);  //Fails at this point ->java: variable ans might not have been initialized
+
+    }
 }
