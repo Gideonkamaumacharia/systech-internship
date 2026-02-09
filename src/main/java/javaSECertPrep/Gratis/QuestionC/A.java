a3ac744f6396976978b2e4eb6ea37bef9f836572
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-09 21:29:25 +0300

    ../../javaSECertPrep/Gratis/QuestionC/

--- src/main/java/javaSECertPrep/Gratis/QuestionC/A.java
+++ src/main/java/javaSECertPrep/Gratis/QuestionC/A.java
@@ -1,4 +1,19 @@
 package javaSECertPrep.Gratis.QuestionC;
 
 public class A {
+
+    static void main() {
+        int x = 100;
+        int a = x++;
+        int b = ++x;
+        int c = x++;
+        //int d = (a<b)? (a<c)?a : (b <c) ? b: c;
+        /**
+         * “If a is less than b, then check if a is less than c.
+         * If yes, return a.
+         * Otherwise, return the result of: if b is less than c, return b, else return c.”
+         *
+         * **/
+        //System.out.println(d);
+    }
 }
