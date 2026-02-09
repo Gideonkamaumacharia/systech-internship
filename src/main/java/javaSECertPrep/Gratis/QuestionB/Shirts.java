2a89bbd27548d235eacae13306884cc0958d0139
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-09 21:28:44 +0300

    ../../javaSECertPrep/Gratis/QuestionB/

--- src/main/java/javaSECertPrep/Gratis/QuestionB/Shirts.java
+++ src/main/java/javaSECertPrep/Gratis/QuestionB/Shirts.java
@@ -1,4 +1,25 @@
 package javaSECertPrep.Gratis.QuestionB;
 
 public class Shirts {
+    static void main() {
+        //Given
+        String shirts [] [] = new String[2][2];
+        shirts [0][0] = "red";
+        shirts [0][1] = "blue";
+        shirts [1][0] = "small";
+        shirts [1][1] = "medium";
+
+        //Which code fragment prints red: blue: small: medium:?
+
+        for(int index = 0;index < 2;){
+            for(int idx = 0; idx < 2;){
+                System.out.println(shirts[index][idx] + ":");
+                idx ++;
+            }
+            index ++;
+        }
+    }
+
+
+
 }
