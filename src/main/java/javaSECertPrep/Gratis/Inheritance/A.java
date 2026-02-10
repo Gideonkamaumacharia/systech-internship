7347b7b7c54f08e29c46b11be058eb7822e666d4
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-10 05:23:07 +0300

    ../../javaSECertPrep/Gratis/Inheritance/A.java

--- src/main/java/javaSECertPrep/Gratis/Inheritance/A.java
+++ src/main/java/javaSECertPrep/Gratis/Inheritance/A.java
@@ -1,4 +1,40 @@
 package javaSECertPrep.Gratis.Inheritance;
 
 public class A {
+    public A(){
+        System.out.println("A");
+    }
 }
+/**
+ * When you create an object of a subclass, Java ALWAYS calls the constructor of its parent first.
+ *
+ * Even if you don’t write it.
+ *
+ * Java secretly inserts super() as the first line of every constructor
+ *
+ * **/
+class B extends A{
+    public B(){
+        System.out.println("B");
+    }
+}
+class C extends B{
+    public C(){
+        System.out.println("C");
+    }
+
+    static void main() {
+        C c = new C();   //ABC is printed
+    }
+}
+
+/**
+ * Java builds objects from top (parent) to bottom (child)
+ *
+ * Parent constructors always run before child constructors
+ *
+ * super() is automatic if you don’t write it
+ *
+ * That’s why output is A → B → C
+ *
+ * **/
