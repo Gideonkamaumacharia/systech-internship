b7a4f3436a4e9e93f1ab2c1bbb49a4eddec9a4af
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-10 05:23:34 +0300

    ../../javaSECertPrep/Gratis/Lists/Lists.java

--- src/main/java/javaSECertPrep/Gratis/Lists/Lists.java
+++ src/main/java/javaSECertPrep/Gratis/Lists/Lists.java
@@ -1,4 +1,20 @@
 package javaSECertPrep.Gratis.Lists;
 
-public class List {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+    static void main() {
+        List<String> names = new ArrayList<>();
+        names.add("Robb");
+        names.add("Bran");
+        names.add("Rick");
+        names.add("Bran");
+
+        if (names.remove("Bran")){
+            names.remove("Jon");
+        }
+        System.out.println(names); //Prints [Robb, Rick, Bran]
+
+    }
 }
