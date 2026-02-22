951f85c9743217d28bbcb5531807081fb770c6e5
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-22 05:35:35 +0300

    ../../javaSECertPrep/Enthuware/Beta.java

--- src/main/java/javaSECertPrep/Enthuware/Beta.java
+++ src/main/java/javaSECertPrep/Enthuware/Beta.java
@@ -1,3 +1,25 @@
-package javaSECertPrep.Enthuware;
-
-
+//package javaSECertPrep.Enthuware;
+//
+//class Baap{
+//    public int h = 4;
+//    public int getH(){
+//        System.out.println("Baap "+h);
+//        return 0;
+//    }
+//
+//    public class Beta extends Baap{
+//        public int h = 44;
+//        return h;
+//    }
+//    public int getH(){
+//        System.out.println("Beta "+h);
+//        return h;     }
+//    public static void main(String[] args) {
+//        Baap b = new Beta();
+//        System.out.println(b.h+" "+b.getH());
+//        Beta bb = (Beta) b;
+//        System.out.println(bb.h+" "+
+//                /44  Beta 44bb.getH());    /
+//
+//    }
+//}
