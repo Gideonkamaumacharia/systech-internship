ef5b7edc6baedaa3051b0211b4025d14055e017f
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-02-22 05:36:22 +0300

    ../../javaSECertPrep/Enthuware/Conversion.java

--- src/main/java/javaSECertPrep/Enthuware/Conversion.java
+++ src/main/java/javaSECertPrep/Enthuware/Conversion.java
@@ -1,4 +1,12 @@
 package javaSECertPrep.Enthuware;
 
-public class Conversion {
+public class Conversion{
+    public static void main(String[] args){
+        int i = 1234567890;
+        float f = i;
+        System.out.println(i - (int)f);
+        System.out.println(f);
+    }
 }
+//Actually it prints -46. This is because the information was lost during the conversion from type int to type float as values of type float are not precise to nine significant digits.
+//Note: You are not required to know the number of significant digits that can be stored by a float for the exam. However, it is good to know about loss of precision while using float and double.
