9f0cb92b21cb08268cfeeb88fa84d6d5ec688b05
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-03-01 21:35:35 +0300

    ../../javaSECertPrep/myExamCloud/PayRoll.java

--- src/main/java/javaSECertPrep/myExamCloud/PayRoll.java
+++ src/main/java/javaSECertPrep/myExamCloud/PayRoll.java
@@ -1,4 +1,18 @@
 package javaSECertPrep.myExamCloud;
 
+import java.util.Vector;
+
 public class PayRoll {
+    static void main() {
+        Vector<String> employeeList =new Vector<>();
+        Vector<Integer> employeeIdList =new Vector<>();
+        Vector<Object> employeeProfileList =new Vector<>();
+
+        System.out.println(employeeList.getClass());
+        System.out.println(employeeProfileList.getClass());
+        System.out.println(employeeIdList.getClass());
+
+    }
+
+
 }
