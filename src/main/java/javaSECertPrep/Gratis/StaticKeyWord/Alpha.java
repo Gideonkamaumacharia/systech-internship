2b217df0bb9087f42b023d62341d1114223499ac
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-03-01 21:34:42 +0300

    ../../javaSECertPrep/Gratis/StaticKeyWord/Alpha.java

--- src/main/java/javaSECertPrep/Gratis/StaticKeyWord/Alpha.java
+++ src/main/java/javaSECertPrep/Gratis/StaticKeyWord/Alpha.java
@@ -1,4 +1,36 @@
 package javaSECertPrep.Gratis.StaticKeyWord;
 
+import java.util.function.Supplier;
+
+//What is the result
 public class Alpha {
+
+    int ns;
+    static int s;
+
+    public Alpha(int ns) {
+        if(s < ns){
+            s = ns;
+            this.ns = ns;
+        }
+
+    }
+
+    void doPrint(){
+        System.out.println("ns: "+ ns + " s: " + s);
+    }
+
+    static void main() {
+        Alpha ref1 = new Alpha(50);
+        Alpha ref2 = new Alpha(125);
+        Alpha ref3 = new Alpha(100);
+
+        ref3.doPrint();
+        ref1.doPrint();
+        ref2.doPrint();
+
+        //ns 50 s 125
+        //ns 125 s 125
+        //ns 0 s 125
+    }
 }
