92951cf6fbc6fc283b4cc2279dcc9d3d3e1cbcbd
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-03-01 21:37:21 +0300

    ../../javaSECertPrep/loops/Geeks.java

--- src/main/java/javaSECertPrep/loops/Geeks.java
+++ src/main/java/javaSECertPrep/loops/Geeks.java
@@ -1,4 +1,11 @@
 package javaSECertPrep.loops;
 
 public class Geeks {
+    static void main() {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 2; j++) {
+                System.out.print(i + "," + j + "j ");//1,1j 1,2j 2,1j 2,2j 3,1j 3,2j
+            }
+        }
+    }
 }
