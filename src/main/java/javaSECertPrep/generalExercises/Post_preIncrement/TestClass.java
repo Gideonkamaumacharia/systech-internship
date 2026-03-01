c21f29e91adc2b7f71f19749bced9fc9985cfaa5
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-03-01 21:36:25 +0300

    ../../javaSECertPrep/generalExercises/Post_preIncrement/TestClass.java

--- src/main/java/javaSECertPrep/generalExercises/Post_preIncrement/TestClass.java
+++ src/main/java/javaSECertPrep/generalExercises/Post_preIncrement/TestClass.java
@@ -1,4 +1,22 @@
 package javaSECertPrep.generalExercises.Post_preIncrement;
 
-public class TestClass {
+public class TestClass{
+    public static void main(String args[ ] ){
+        int i = 0 ;
+        boolean bool1 = true ;
+        boolean bool2 = false;
+        boolean bool  = false;
+        bool = ( bool2 &  method1(++i) ); //1
+        System.out.println(i);
+        bool = ( bool2 && method1(i++) ); //2
+        System.out.println(i);
+        bool = ( bool2 |  method1(--i) ); //3
+        System.out.println(i);
+        bool = ( method1(i++) || bool1); //4
+        System.out.println(i);
+        System.out.println(bool);
+    }
+    public static boolean method1(int i){
+        return i>0 ? true : false;
+    }
 }
