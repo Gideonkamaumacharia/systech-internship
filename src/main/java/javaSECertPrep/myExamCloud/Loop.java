edd5fba6c742f6baa687184e580e36f38fbd52ea
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-03-01 21:35:59 +0300

    ../../javaSECertPrep/myExamCloud/Loop.java

--- src/main/java/javaSECertPrep/myExamCloud/Loop.java
+++ src/main/java/javaSECertPrep/myExamCloud/Loop.java
@@ -1,4 +1,21 @@
 package javaSECertPrep.myExamCloud;
 
 public class Loop {
+
+    static void main() {
+         boolean keepGoing = true;
+         int count = 0;
+         int x = 3;
+         while(count++ < 3){
+             int y = (1 + 2* count) % 3;
+             switch(y){
+                 default:
+                 case(0):x-=1;break;
+                 case(1):x+=5;
+
+             }
+         }
+        System.out.println(x);
+
+    }
 }
