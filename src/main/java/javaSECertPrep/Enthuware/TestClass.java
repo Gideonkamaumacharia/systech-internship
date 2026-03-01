dacc6f630012a9875ee242d50c9a64a7dc6cd814
Gideonkamaumacharia <kamaugedion95@gmail.com>
2026-03-01 21:33:49 +0300

    ../../javaSECertPrep/Enthuware/TestClass.java

--- src/main/java/javaSECertPrep/Enthuware/TestClass.java
+++ src/main/java/javaSECertPrep/Enthuware/TestClass.java
@@ -1,4 +1,21 @@
 package javaSECertPrep.Enthuware;
 
-public class TestClass {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class TestClass{
+    public static void main(String[] args) {
+        System.out.println(getMsg((char)10));      }
+
+    @NotNull
+    public static String getMsg(char x){
+        String msg = "Input value must be ";
+        msg = msg.concat("smaller than X");
+        msg.replace('X', x);
+        String rest = " and larger than 0";
+        msg.concat(rest);
+        return msg;
+    }
 }
